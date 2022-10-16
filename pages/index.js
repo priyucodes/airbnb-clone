ae91e252180b12648a4d30dfa5ce4f721c300ed0
Priyanshu Bhandari <technopriyu@gmail.com>
2022-10-17 01:46:13 +0530

    added banner

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Banner />
     </>
   )
 }
